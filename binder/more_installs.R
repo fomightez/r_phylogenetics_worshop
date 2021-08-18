7694a01d14a213c588c598f408aeaa5298d55bf3
fomightez <wayne4.antispam@server.fake>
2021-08-18 13:12:59 -0400

    attempting to deal with treeio saying it needs 4.1 by moving to R installing treeio instead of conda

--- binder/more_installs.R
+++ binder/more_installs.R
@@ -3,3 +3,4 @@
 install.packages("ggtree", repos='http://cran.us.r-project.org') # adding this here because needs to install but trying with conda, I was seeing
 # "package bioconductor-ggtree-3.0.1-r41hdfd78af_0 requires r-base >=4.1,<4.2.0a0, but none of the providers can be installed", but also saw when I
 # I tried updating to r-base 4.1, still was seeing, and so trying this. This had helped with a package before, see https://github.com/fomightez/chapter7/blob/master/binder/more_installs.R
+install.packages("treeio", repos='http://cran.us.r-project.org') # same as for ggtree
